af96c159f02a5cbd8e4ab42c61b8b6392d6133ba
Martin NADAUD <manadaud2@local.isima.fr>
2024-02-15 11:54:47 +0100

    test

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+L=[1,2,3]
+#juste avec +
+def f(L):
+    L=[1,2,3]
+    for i in range (len(L)):
+        u=L[i]+L[i+1]
+        L.append(u)
+    return L
+
+print(f(L))    
+
