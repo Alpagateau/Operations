6c8c25ba5382b806f25f80092d59f9f698e43670
Clerie POZIN-ROUX <clpozinrou@local.isima.fr>
2024-02-20 17:22:09 +0100

    seance 20_02

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-L=[1,2,3]
-#juste avec +
-def f(L):
-    L=[1,2,3]
+L=[2,3,4,9,6,7]
+
+def somme_simple(L, i, k): #0001
+    u=L[i]+L[k]
+    L.pop(k)
+    L.pop(i)
+    L.append(u)
+    return L
+
+
+#avec + :
+def plus(L):
+    A=L.copy()       
+    D=[]                     #0001 -> L [...]
     for i in range (len(L)):
         for k in range (len(L)):
             if i==k:
@@ -9,9 +19,60 @@ def f(L):
             elif i > k:
                 continue
             else:
-                u=L[i]+L[k]
-                L.append(u)
-    return L
+                A = L.copy()
+                somme_simple(A, i, k)#00ik -> L[...]
+                D.append(A)
+    return D
 
-print(f(L))    
+print(plus(L))
 
+
+
+#avec multiplication 
+
+def multiplication(L):
+    B=L.copy()
+    C = []
+    for j in range(len(L)):
+        for  n in range (len(L)):
+            if j==n:
+                continue
+            if j>n:
+                continue
+            if j<n:
+                B=L.copy()
+                v=B[j]*B[n]
+                B.pop(n)
+                B.pop(j)
+                B.append(v)
+                C.append(B)     
+    return C
+
+print(multiplication(L))
+
+
+def soustraction(L):
+    E=L.copy()
+    F = []
+    for j in range(len(L)):
+        for  n in range (len(L)):
+            if j==n:
+                continue
+            if j>n:
+                continue
+            if j<n:
+                E=L.copy()
+                v=E[j]-E[n]
+                E.pop(n)
+                E.pop(j)
+                E.append(v)
+                F.append(E)     
+    return F
+
+
+#print(f(L,0,1))    
+
+'''A=[0,9,5,2,3,7,9,4,5]
+B=sorted(A)
+
+print(B)'''
